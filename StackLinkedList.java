e6d3c7f6e657913b2951298cb686ca3ff60ef815
sknithin06 <nithinsk939@gmail.com>
2021-07-17 09:24:16 +0530

    create a stack using linkedlist

--- StackLinkedList.java
+++ StackLinkedList.java
@@ -1,6 +1,59 @@
 package bridgelabz;
 public class StackLinkedList {
+	class Node {
+		int num;
+		Node link;
+	}
+	Node top;
+	StackLinkedList() {
+		this.top =null;
+	}
+	public void push(int x) 
+    {
+        Node value = new Node();
+        if (value == null) {
+            System.out.print("Heap Overflow");
+            return;
+        }
+        value.num = x;
+        value.link = top;
+        top = value;
+    }
+	public boolean isEmpty() {
+	    return top == null;
+	}
+	public int peek() {
+	    if (!isEmpty()) {
+	        return top.num;
+	    }
+	    else {
+	        System.out.println("Stack is empty");
+	        return -1;
+	    }
+	}
+	 public void display()
+	    {    
+		 if (top == null) {
+			 System.out.printf("\nStack Underflow");	        
+	     }
+	     else {
+	    	 Node value = top;
+	         while (value != null) {
+	                System.out.printf("%d->", value.num);
+	                value = value.link;
+	            }
+	    }
+	}
 	public static void main(String[] args) {
 		System.out.println("Welcome to Stack Problems");
+		
+		StackLinkedList list = new StackLinkedList();
+		list.push(70);
+		list.push(30);
+		list.push(56);
+		
+		list.display();
+		System.out.printf("\nTop element is %d\n", list.peek());
+		
 	}
 }
